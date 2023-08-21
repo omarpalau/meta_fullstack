cbec1488e6d5e2256fe2edbe4484fcc48f274c94
omarpalau <omar.palau@wizeline.com>
2023-08-21 17:00:42 -0600

    Connected MySQL database

--- LittleLemon/myproject/settings.py
+++ LittleLemon/myproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'HOST':'127.0.0.1',
+        'USER':'root',
+        'PASSWORD':'root',
+        'PORT':'3306'
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # meta_fullstack
+
+This is version control and note taking repository for the Meta FullStack Coursera course.
+
+## Prerequisites:
+
+- The MySQL user/password is set to "root/root"
+- You need to create a MySQL database called 'littlelemon'
